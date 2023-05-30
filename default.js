429e1c62f14f21cd633ecbddd3f8f79a6308c250
JunYoung_Kim <hjklo0220@gmail.com>
2023-05-30 11:24:26 +0900

    JS 출력함수 실습

--- default.js
+++ default.js
@@ -1,2 +1,14 @@
 
 document.write('hello world2');
+document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
+
+console.log("Welcome JS Program");
+console.info("Welcome JS Program"); //기본
+console.warn("Welcome JS Program");
+console.error("Welcome JS Program");
+
+alert('Welcome JS Program')
+var a = prompt('Welcome JS Program')
+console.log(a)
+
