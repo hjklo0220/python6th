fd590690b976383f8662ff063a6f7287651bc37f
JunYoung_Kim <hjklo0220@gmail.com>
2023-05-30 13:03:19 +0900

    데이터 타입, 주석 실습

--- default.js
+++ default.js
@@ -8,7 +8,27 @@ console.info("Welcome JS Program"); //기본
 console.warn("Welcome JS Program");
 console.error("Welcome JS Program");
 
-alert('Welcome JS Program')
-var a = prompt('Welcome JS Program')
-console.log(a)
+//alert('Welcome JS Program');
+//var a = prompt('Welcome JS Program');
+//console.log(a);
 
+console.log(123, typeof 123);    // number
+console.log(123.5, typeof 123.5);  // number
+console.log('123', typeof '123');  // string
+console.log(true, typeof true);   //boolean
+console.log(false, typeof false);  //boolean
+
+var car;
+console.log(typeof car);    //undefined - 아무것도 없음
+var car = ""
+console.log(typeof car);    //string
+console.log(isNaN());       //NaN - 잘못된 입력을 받았음을 나타내는 기호
+var person = {
+    firstName : 'John',
+    lastName: 'Doe',
+    age : 50,
+    eyeColor : "blue"
+    };
+console.log(typeof person, person); //object
+person = null; //null - '값 없음' 이라는 값이 있음
+console.log(typeof person, person); //object
