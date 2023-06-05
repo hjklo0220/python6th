97ce57b6d4c8fd71dbba506c6590e461bbe01034
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-05 11:30:52 +0900

    산술 연산자 실습

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-# 변수 선언
-a = 10
-b = 3.14
-c = 'hello world'
-d = [1, 2, 3]
-e = (4, 5, 6)  # 튜플
-f = {7, 8, 9}  # set(집합) 유니크 보장되어있음
-g = {"apple": 1,
-     "banana": 2,
-     "orange": 3
-     }
+# # 변수 선언
+# a = 10
+# b = 3.14
+# c = 'hello world'
+# d = [1, 2, 3]
+# e = (4, 5, 6)  # 튜플
+# f = {7, 8, 9}  # set(집합) 유니크 보장되어있음
+# g = {"apple": 1,
+#      "banana": 2,
+#      "orange": 3
+#      }
+#
+# print(type(a), a)
+# print(type(b), b)
+# print(type(c), c)
+# print(type(d), d)
+# print(type(e), e)
+# print(type(f), f)
+# print(type(g), g)
 
-print(type(a), a)
-print(type(b), b)
-print(type(c), c)
-print(type(d), d)
-print(type(e), e)
-print(type(f), f)
-print(type(g), g)
+# 연산자
+a = 4
+b = 2
+total = a + b
+print("a + b = ", total)
+total = a - b
+print("a - b = ", total)
+total = a * b
+print("a * b = ", total, type(total))
+print("a / b = ", total, type(a / b))
+
+a = 5
+b = 2
+print("a % b = ", a % b)
+print("a ** b = ", a ** b)
+print("a // b = ", a // b) # 몫(양수)
+a = -5
+print("a // b = ", a // b) # 몫(음수)
+
