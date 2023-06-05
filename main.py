9cd056c36ca796c97e1e7b4707284cb6df73e3dd
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-05 10:23:30 +0900

    파이썬 변수 실습

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+# 변수 선언
+a = 10
+b = 3.14
+c = 'hello world'
+d = [1, 2, 3]
+e = (4, 5, 6)  # 튜플
+f = {7, 8, 9}  # set(집합) 유니크 보장되어있음
+g = {"apple": 1,
+     "banana": 2,
+     "orange": 3
+     }
 
-print('Hello World')
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
+print(type(e), e)
+print(type(f), f)
+print(type(g), g)
