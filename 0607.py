d907629fce87838718ad1d762027eced89d57fc7
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-07 11:40:46 +0900

    배열 추가/삭제

--- 0607.py
+++ 0607.py
@@ -141,32 +141,60 @@
 # 	print("6보다 큼")	# TODO 작업 진행
 
 
-# 배열
-import array
-stu_roll = array.array('i', [101, 102, 103, 104, 105])
-print(stu_roll[0])
-print(stu_roll[1])
-print(stu_roll[2])
-print(stu_roll[3])
-print(stu_roll[4])
+# # 배열
+# import array
+# stu_roll = array.array('i', [101, 102, 103, 104, 105])
+# print(stu_roll[0])
+# print(stu_roll[1])
+# print(stu_roll[2])
+# print(stu_roll[3])
+# print(stu_roll[4])
+#
+# print("for in")
+# for ele in stu_roll:
+# 	print(ele)
+#
+# print("index를 이용")
+# n = len(stu_roll)
+# for i in range(n):
+# 	print(i, "=", stu_roll[i])
+#
+# print("while")
+# i = 0
+# while i < n:
+# 	print(i, "=", stu_roll[i])
+# 	i += 1
 
-print("for in")
-for ele in stu_roll:
-	print(ele)
-
-print("index를 이용")
+#
+from array import *
+stu_roll = array('i', [101, 102, 103, 104, 105])
 n = len(stu_roll)
-for i in range(n):
-	print(i, "=", stu_roll[i])
-
-print("while")
 i = 0
 while i < n:
-	print(i, "=", stu_roll[i])
+	print(stu_roll[i])
 	i += 1
 
+print("Array insert")
+stu_roll.insert(1, 106)
+stu_roll.insert(3, 107)
+n = len(stu_roll)
+i = 0
+while i < n:
+	print(stu_roll[i])
+	i += 1
 
+print("Array remove")
+stu_roll.remove(101)
+n = len(stu_roll)
+i = 0
+while i < n:
+	print(stu_roll[i])
+	i += 1
 
-
-
-
+print("Array pop")
+stu_roll.pop(2)
+n = len(stu_roll)
+i = 0
+while i < n:
+	print(stu_roll[i])
+	i += 1
