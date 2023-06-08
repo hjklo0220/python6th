026c0204cb10ba32ca42f6d9d352e40df137339f
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-08 10:15:09 +0900

    함수 파라미터 활용

--- 0608.py
+++ 0608.py
@@ -1,1 +1,19 @@
+# 함수
+def pw(x, y):
+	z = x ** y
+	print(z)
 
+
+pw(2, 5)
+
+
+# pw(5, 2, 3) 파라미터 하나 더 받으면 오류
+
+def show(name, age=30):
+	print(f"name: {name}, Age: {age}")
+
+
+show(age=90, name="test")
+show(name="hello")
+
+# 리스트
