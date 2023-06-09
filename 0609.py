f314cce4f07763a0c810ce779a2c561120106945
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-09 14:02:47 +0900

    readlines()

--- 0609.py
+++ 0609.py
@@ -154,17 +154,56 @@ import time
 # file_object.close()
 
 
-# 파일 열기
-file_object = open('example.txt', 'r')
+# # 파일 열기
+# file_object = open('example.txt', 'r')
+#
+# print("현재 파일 위치 확인")
+# position = file_object.tell()
+# print("Current position : ", position)
+#
+# print("파일 포인터 위치 변경")
+# file_object.seek(7)
+#
+# position = file_object.tell()
+# print("변경된 위치 : ", position)
+#
+# file_object.close()
 
-print("현재 파일 위치 확인")
-position = file_object.tell()
-print("Current position : ", position)
 
-print("파일 포인터 위치 변경")
-file_object.seek(7)
+# # with - 자동으로 close됨
+# with open('example.txt', 'w') as file_object:
+# 	content = '''asfkjqeogheqjhgfqe
+# dfjaslfjaslkjdwqinjnjj
+# askdjlasjdklasjfidsfj;gj
+# lkfjasldfjsljfoajiwieiweoooo
+# aslkfnsldfsnkdnfkldsajflkjas
+# 	'''
+# 	file_object.write(content)
 
-position = file_object.tell()
-print("변경된 위치 : ", position)
+# readlines()
+with open('example.txt', 'r+') as file_object:
+	lines = file_object.readlines()
+	for line in lines:
+		print('>', line.strip())
 
-file_object.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
