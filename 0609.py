bce3bbbd987c02d22dd3de455dc463cb4150bfdf
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-09 10:30:29 +0900

    오버로딩, 오버라이딩

--- 0609.py
+++ 0609.py
@@ -1,28 +1,60 @@
-# 클래스 네임스페이스, 인스턴스 네임스페이스 차이
-class Moblie:
-	fp = "yes"
+# # 클래스 네임스페이스, 인스턴스 네임스페이스 차이
+# class Moblie:
+# 	fp = "yes"
+#
+#
+# realme = Moblie()  # 생성자 함수, 이 클래스를
+# redme = Moblie()
+# geek = Moblie()
+#
+# print(Moblie.fp)
+# print(realme.fp)
+# print(redme.fp)
+# print(geek.fp)
+#
+#
+# Moblie.fp = 'no'  # 클래스 네임스페이스, 값이 다바뀜
+#
+# print(Moblie.fp)
+# print(realme.fp)
+# print(redme.fp)
+# print(geek.fp)
+#
+# realme.fp = 'realme change'  # 인스턴스 네임스페이스, 이 값만 바뀜
+#
+# print(Moblie.fp)
+# print(realme.fp)
+# print(redme.fp)
+# print(geek.fp)
 
 
-realme = Moblie()  # 생성자 함수, 이 클래스를
-redme = Moblie()
-geek = Moblie()
+# 메소드 오버라이딩
 
-print(Moblie.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
+class Vector(object):
+	def __init__(self, x, y):  # 생성자 함수 Vector() 하면 실행되는거임
+		self.x = x
+		self.y = y
+
+	def __add__(self, other):  # 연산자 오버로딩 ,정의를 해야만 쓸수 있음 오브젝트 클래스에 정의가 안되어있음
+		return Vector(self.x + other.x, self.y + other.y)
+
+	def __str__(self):  # 오브젝트 클래스에 정의되어있음, 새로 내맘대로 정의한것임 -> 오버라이딩
+		return f'Vertor({self.x}, {self.y})'
 
 
-Moblie.fp = 'no'  # 클래스 네임스페이스, 값이 다바뀜
 
-print(Moblie.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
+a = Vector(1, 2)  # 인스턴스 네임스페이스
+b = Vector(3, 4)
 
-realme.fp = 'realme change'  # 인스턴스 네임스페이스, 이 값만 바뀜
+print(a)
+print(b)
 
-print(Moblie.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
+c = a + b
+
+print(c)
+
+
+
+
+
+
