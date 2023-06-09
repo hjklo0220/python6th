7a4d9328f7fa72035ae0ecbb64dd5de8c8bdcd60
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-09 11:04:28 +0900

    time 모듈

--- 0609.py
+++ 0609.py
@@ -26,32 +26,73 @@
 # print(realme.fp)
 # print(redme.fp)
 # print(geek.fp)
+import time
+# # 메소드 오버라이딩
+#
+# class Vector(object):
+# 	def __init__(self, x, y):  # 생성자 함수 Vector() 하면 실행되는거임
+# 		self.x = x
+# 		self.y = y
+#
+# 	def __add__(self, other):  # 연산자 오버로딩 ,정의를 해야만 쓸수 있음 오브젝트 클래스에 정의가 안되어있음
+# 		return Vector(self.x + other.x, self.y + other.y)
+#
+# 	def __str__(self):  # 오브젝트 클래스에 정의되어있음, 새로 내맘대로 정의한것임 -> 오버라이딩
+# 		return f'Vertor({self.x}, {self.y})'
+#
+#
+#
+# a = Vector(1, 2)  # 인스턴스 네임스페이스
+# b = Vector(3, 4)
+#
+# print(a)
+# print(b)
+#
+# c = a + b
+#
+# print(c)
 
 
-# 메소드 오버라이딩
+# time 모듈
+# import time
+# print(time.time())
+# print(time.ctime())
 
-class Vector(object):
-	def __init__(self, x, y):  # 생성자 함수 Vector() 하면 실행되는거임
-		self.x = x
-		self.y = y
+from datetime import datetime
+from datetime import date
 
-	def __add__(self, other):  # 연산자 오버로딩 ,정의를 해야만 쓸수 있음 오브젝트 클래스에 정의가 안되어있음
-		return Vector(self.x + other.x, self.y + other.y)
+dt = datetime(year=2023, month=5, day=5, hour=10, minute=30)
+print(dt, type(dt))
 
-	def __str__(self):  # 오브젝트 클래스에 정의되어있음, 새로 내맘대로 정의한것임 -> 오버라이딩
-		return f'Vertor({self.x}, {self.y})'
+current_time = time.ctime()
+current_datetime = datetime.now()
+print(current_time, current_datetime)  # 날짜 시간 전부
+
+d = date(year=2023, month=6, day=25)
+print(d)  # 날짜만
+
+current_date = date.today()
+print(current_date)
 
 
 
-a = Vector(1, 2)  # 인스턴스 네임스페이스
-b = Vector(3, 4)
 
-print(a)
-print(b)
 
-c = a + b
 
-print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
