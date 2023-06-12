1204a750aedebc15bbc60899a28e12e706f625d1
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-12 15:46:08 +0900

    dictionary 실습

--- 0612.py
+++ 0612.py
@@ -133,7 +133,26 @@
 # for i, v in enumerate(a):
 # 	print(i, v)
 
+#
+# import pprint
+#
+# pprint.pprint(locals())
 
-import pprint
 
-pprint.pprint(locals())
+# dictionary
+
+words = ['apple', 'bat', 'bar', 'atom', 'book']
+by_letters = {}
+
+for word in words:
+	letter = word[0]
+	if letter not in by_letters:
+		by_letters[letter] = [word]
+	else:
+		by_letters[letter].append(word)
+
+print(by_letters)
+# print(by_letters['c'])
+
+
+
