841336c29a008d6877c4fae257d46e40c6b16b65
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-12 10:32:21 +0900

    상속 실습

--- 0612.py
+++ 0612.py
@@ -1,30 +1,55 @@
 
-# 클래스
-class Car:
-	wheels = 4	# 클래스 속성
+# # 클래스
+# class Car:
+# 	wheels = 4	# 클래스 속성
+#
+# 	def __init__(self, make, model, color):
+# 		self.make = make
+# 		self.model = model
+# 		self.color = color
+#
+# 	# Method
+# 	# @staticmethod  ???
+# 	def drive(self):
+# 		return "the car is moving"
+#
+# 	def stop(self):
+# 		return f"the car {self.make} has stopped"
+#
+#
+# my_car = Car("kia", "morning", "blue")
+# print(my_car)
+#
+# #속성 사용하기
+# print(my_car.make)
+# print(my_car.wheels)
+#
+#
+# #메소드 호출
+# print(my_car.drive())
+# print(my_car.stop())
 
-	def __init__(self, make, model, color):
+
+# 부모 클래스
+class Vehicle:
+	def __init__(self, make, model, year):
 		self.make = make
 		self.model = model
-		self.color = color
+		self.year = year
 
-	# Method
-	# @staticmethod  ???
-	def drive(self):
-		return "the car is moving"
-
-	def stop(self):
-		return f"the car {self.make} has stopped"
+	def start_engine(self):
+		return "the engine is running"
 
 
-my_car = Car("kia", "morning", "blue")
-print(my_car)
-
-#속성 사용하기
-print(my_car.make)
-print(my_car.wheels)
+class Car(Vehicle):
+	def start_engine(self):
+		return super().start_engine() + f" It's a {self.make} car engine"
 
 
-#메소드 호출
-print(my_car.drive())
-print(my_car.stop())
+#인스턴스 생성
+my_car = Car("kia", "k3", "2023")
+
+print(my_car.start_engine())
+
+
+
