0eca689d7028cc920f0570466f97a2d2c2a33ae0
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-20 15:34:49 +0900

    Question, Choice 모델 클래스 생성

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+	questioin_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('data published')
+
+	def __str__(self):
+		return self.questioin_text
+
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField  # 문항을 선택한 사용자 수 입력받을 변수
+
+	def __str__(self):
+		return self.choice_text
