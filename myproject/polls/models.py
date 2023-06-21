9b1bec0ed52dc55ffafc7dfd28f3051df3729cef
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-21 19:51:16 +0900

    view, template 작성

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-	questioin_text = models.CharField(max_length=200)
+	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('data published')
 
 	def __str__(self):
-		return self.questioin_text
+		return self.question_text
 
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField  # 문항을 선택한 사용자 수 입력받을 변수
+	votes = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.choice_text
